8e6fd3bac07fc5352aadcf79e5ccbe948c433c4e
Shirly Manor <shirlymanor@macbookpro.lan>
2024-11-21 10:47:08 -0800

    fixing typos

--- example_usage.py
+++ example_usage.py
@@ -2,7 +2,7 @@ from pokemon_sdk import PokeSDK
 '''
 example of how to use the sdk'''
 client = PokeSDK()
-pokemon = client.get_pokemon('pikachu')
+pokemon = client.pokemon.get_pokemon('pikachu')
 print(pokemon['name'])
 
 # print ability name
@@ -11,9 +11,9 @@ for ability in pikachu['abilities']:
     print(ability['ability']['name'])
 
 generation_list = client.generation.get_generations_list(limit=3,offset=0)
-for gen in generation_list['reslts']:
+for gen in generation_list['results']:
     print(gen['name'])
 
 geneartion_i = client.generation.get_generation('generation-i')
-for species in geneartion_i['species']:
+for species in geneartion_i['pokemon_species']:
     print(species['name'])
